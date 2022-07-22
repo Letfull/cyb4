4ff22e5591ee9b756be59e5af464f9622404263a
letfull <rin89at>
2022-07-22 16:25:03 +0300

    Initial commit

---
+++ doubler.php
@@ -0,0 +1,7 @@
+<a href="start.html">Домашняя страница</a>
+<?php
+	$x = $_REQUEST["data"];
+	$result = 2 * $x;
+
+	echo "Результат удвоения числа $x есть $result";
+?>
---
+++ registration.php
@@ -0,0 +1,30 @@
+<html>
+	<head>
+		<title>Регистрация</title>
+		<meta charset="utf8" />
+		<style>
+			input, button {
+			width:150px;
+			margin:1px;
+			text-align:center;
+			}
+		</style>
+	</head>
+	
+	<body>
+		<a href="start.html">Возврат на стартовую страницу</a></br>
+		<h2>Пожайлуста, зарегистрируйтесь, введя имя пользователя, адрес электронной почты и пароль.</h2>
+		<form method="POST" action="check_registration.php">
+			<a>Email:</a></br>
+			<a><input type="email" name="Email"/></a></br>
+			<p><a>Имя пользователя:</a></br>
+			<a><input type="text" name="txtUser"/></a></br>
+			<p><a>Пароль:</a></br>
+			<input type="password" name="txtPwd"/></a></br>
+			<p><a>Повторите пароль:</a></br>
+			<input type="password" name="txtPwd_double"/></p></br>
+			<button>Регистрация</button></br>
+			<em>Автор: Летфуллин Ринат</em>
+		</form>
+	</body>
+</html>
